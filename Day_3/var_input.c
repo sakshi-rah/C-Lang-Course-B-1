ef2aade25f033ad409313e8b6e9987d03446d849
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2025-05-17 10:32:43 +0530

    var_input_day3 file added

--- Day_3/var_input.c
+++ Day_3/var_input.c
@@ -1,24 +1,29 @@
 #include<stdio.h>
 
 int main(){
-//Input fron user using scanf() predefined function
-int num1;
-printf("Enter your num1 value: \n");
-scanf("%d", &num1);
-printf("num1 value : %d", num1);
+//Input fron user using scanf() predefined function40
+// int num1;
+// printf("Enter your num1 value: ");
+// scanf("%d", &num1);
+// printf("num1 value : %d\n", num1);
 
 int x;
 printf("Enter x value:");
 scanf("%d", &x);
-printf("value of x : %d", x);
+printf("value of x : %d\n", x);
 
 int y;
 printf("Enter y value:");
 scanf("%d", &y);
-printf("value of y : %d", y);
+printf("value of y : %d\n", y);
 
 int sum = x+y;
-printf("sum of x and y is %d", sum);
+int sub = x-y;
+int multi = x*y;
+printf("sum of x and y is %d\n", sum);
+printf("sub of x and y is %d\n", sub);
+printf("multi of x and y is %d\n", multi);
 
 return 0;
+
 }
