5fae0fb6fe8c591e5d2222ce602b516b76486f6e
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2025-05-19 21:13:10 +0530

    Operators_day-5

---
+++ Day_5/oprators.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+//  1. Arithmetic operators (+,-,*,/,%)
+//  2. Increment Decrement Operators(++,--)
+//  3. Assignment operators (=,+=,-=,*=,/=,%=)
+//  4. Relational Operators (<,<=,>,>=,==,!=)
+//  5. Logical operators (&&,||,!)
+//  6. Bitwise operators (&,<<,>>,^,|,~)
+
+// int num1 = 87;
+// int num2 = 6;
+// int num3 = 34;
+// int sum = num1+num2+num3; //30
+
+// printf("Sum of three numbers %d\n", sum); // 79
+
+printf("Incre and Decre Operator\n");
+
+int x = 5;
+
+printf("x is %d\n", x); // 5
+printf("x++ value is %d\n", x++); //print x value first = 5 then incre by 1 = 6 // x = 6
+printf("x-- value is %d\n", x--); //print x value first = 6 then decre by 1 = 5 // x = 5
+printf("--x value is %d\n", --x); // 4
+printf("++x value is %d\n", ++x); // 5
+printf("x++ value is %d\n", x++); // 5 // 6
+printf("%d\n", x);
+    return 0;
+
+
+    
+}
