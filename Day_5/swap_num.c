e73402dd92b50895157059e08666c72dc92ba3b2
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2025-05-24 20:53:15 +0530

    added Day_5

---
+++ Day_5/swap_num.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main(){
+// // Swapping Two Numbers    
+// int a = 5; // a = 8
+// int b = 8; // b = 5
+// printf("a = %d, b = %d \n", a, b);
+// int x;
+// x = a;
+// a = b;
+// b = x;
+// printf("a = %d, b = %d \n", a, b);
+
+//ASCII Values keyboard= 127
+char A = 'A'; // A = 65
+printf("ASCII Values of A = %d\n", A);
+char Z = 'Z'; // A = 65
+printf("ASCII Values of Z = %d\n", Z);
+
+// 65+32 = 97
+// A+32 = a 
+// 90+32 = 122
+// Z + 32 = z
+char a = 'a'; // a = 97-32 = 65
+printf("ASCII Values of a = %d\n", a);
+char z = 'z'; // a = 122
+printf("ASCII Values of z = %d\n", z);
+
+int num = 76;
+printf("ASCII num = %c", num);
+
+    return 0;
+}
